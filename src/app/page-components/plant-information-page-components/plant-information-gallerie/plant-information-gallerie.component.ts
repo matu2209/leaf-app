c955e501f5679efcfd8f40ead1eaaabcb8f0930a
JoshuaBaccaro <joshuabaccaro@gmail.com>
2024-11-11 12:24:08 -0300

    Mejoras visuales
    Se mejoro el scss de la pagina del perfil y se añadio la funcionalidad de expandir imagenes en los registros de las plantas

--- src/app/page-components/plant-information-page-components/plant-information-gallerie/plant-information-gallerie.component.ts
+++ src/app/page-components/plant-information-page-components/plant-information-gallerie/plant-information-gallerie.component.ts
@@ -1,12 +1,21 @@
 import { Component, Input } from '@angular/core';
 
+declare var bootstrap: any; 
 @Component({
   selector: 'app-plant-information-gallerie',
   templateUrl: './plant-information-gallerie.component.html',
-  styleUrl: './plant-information-gallerie.component.scss'
+  styleUrls: ['./plant-information-gallerie.component.scss']
 })
+
 export class PlantInformationGallerieComponent {
 
-  @Input() plantData: any;  // Recibir la planta desde el componente padre
+  @Input() plantData: any; 
+  selectedImage: string = ''; 
 
+
+  openImageModal(imageUrl: string) {
+    this.selectedImage = imageUrl; 
+    const modal = new bootstrap.Modal(document.getElementById('imageModal')); 
+    modal.show();
+  }
 }
